6360b774d3bde6a04b6d2846e0a23e8edd2306fe
Jack Erekson <jackerekson@Jacks-MacBook-Air.local>
2021-08-31 14:54:02 -0500

    adding rollbar

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const path = require('path')
+const Rollbar = require('rollbar')
+
+let rollbar = new Rollbar({
+    accessToken: 'e3635c339ce84e63b551ee7da8c88474',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 
 const app = express()
 
